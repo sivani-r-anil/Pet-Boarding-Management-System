beb44476bb9f6446e4533604be7d9a247252b2f8
sivani-r-anil <sivaniranil28@gmail.com>
2026-07-15 03:19:14 +0530

    View  pets added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddPet from './components/AddPet'
+import ViewPets from './components/ViewPets'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <AddPet />
+      <ViewPets/>
     </>
   )
 }
